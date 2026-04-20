eaa64b2a5b9e5a5e7cc8a1a7d6b90d1f13b2a3d0
Shubham P Prateek <pratik.shub@gmail.com>
2026-04-20 17:12:51 +0530

    Add: majority-element - submission-0

---
+++ Data Structures & Algorithms/majority-element/submission-0.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        int res=0, count=0;
+
+        for(int i:nums){
+            if(count==0){
+                res = i;
+            }
+
+            count+= (res==i)? 1:-1;
+        }
+        return res;
+    }
+}
